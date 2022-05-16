428c3ba6deb89b4e48d1d1705eb7603be7a217c4
Sean Aery <sean.aery@duke.edu>
2022-05-16 09:01:24 -0400

    More Rails 5->6 updates resulting from running bin/rails app:update.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
   mount Blacklight::Engine => '/'
   mount BlacklightDynamicSitemap::Engine => '/'
